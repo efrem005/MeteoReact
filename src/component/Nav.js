68d4c80f2c8c76a04e3274221bb06ca1ae270adf
= <=>
2021-11-08 05:06:49 +0700

    manifest version

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -5,7 +5,7 @@ export default function Nav({count, handleChange, handleSubmit, loading}) {
     return (
         <div className="navbar navbar-expand-lg navbar-light bg-light mb-3 text-center">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">
+                <a className="navbar-brand" href="/MeteoReact">
                     <img style={{height: 50}} src={logo} alt="метеоСтанция"/></a>
                 <button
                     className="navbar-toggler"
@@ -19,17 +19,17 @@ export default function Nav({count, handleChange, handleSubmit, loading}) {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <button className="btn btn-outline-success disabled">Главная</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className="btn btn-outline-success mx-2 disabled">18B20</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className="btn btn-outline-success disabled">BME280</button>
-                        </li>
-                    </ul>
+                    {/*<ul className="navbar-nav mr-auto mb-2 mb-lg-0">*/}
+                    {/*    <li className="nav-item">*/}
+                    {/*        <button className="btn btn-outline-success disabled">Главная</button>*/}
+                    {/*    </li>*/}
+                    {/*    <li className="nav-item">*/}
+                    {/*        <button className="btn btn-outline-success mx-2 disabled">18B20</button>*/}
+                    {/*    </li>*/}
+                    {/*    <li className="nav-item">*/}
+                    {/*        <button className="btn btn-outline-success disabled">BME280</button>*/}
+                    {/*    </li>*/}
+                    {/*</ul>*/}
                     <form onSubmit={handleSubmit} className="d-flex justify-content-center">
                         <div className="col-auto mr-2" style={{width: 215}}>
                             <select className="form-select" id="autoSizingSelect" onChange={handleChange}>
